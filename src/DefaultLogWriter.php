3dd46f5fb07c18204b29d67aae1bacf7e3d193ea
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-13 22:18:34 +0100

    main | Change implementation of logRequest method, add response & startTime arguments, and Add filePath method to make new log folder structure, and Change data return of getMessage method, with more request & response information - src\DefaultLogWriter.php

--- src/DefaultLogWriter.php
+++ src/DefaultLogWriter.php
@@ -3,43 +3,81 @@
 namespace Spatie\HttpLogger;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Illuminate\Support\Facades\File;
+use Spatie\HttpLogger\Supports\JsonLogFileWriter;
 
 class DefaultLogWriter implements LogWriter
 {
+    const FORMAT_JSON = 'json';
+
     protected $sanitizer;
 
-    public function logRequest(Request $request)
+    public function logRequest(Request $request, Response $response, $startTime)
     {
-        $message = $this->formatMessage($this->getMessage($request));
+        $message = $this->getMessage($request, $response, $startTime);
 
-        Log::channel(config('http-logger.log_channel'))->log(config('http-logger.log_level', 'info'), $message);
+        if (self::FORMAT_JSON == 'json' && isset($message)) {
+            (new JsonLogFileWriter)->write($this->filePath(), $message);
+        }
     }
 
-    public function getMessage(Request $request)
+    public function filePath()
+    {
+        $baseDir = storage_path('logs/' .config('http-logger.log_level', 'info'). '/' .date('Y-m'). '//http_requests/');
+    
+        if (!File::exists($baseDir)) {
+            File::makeDirectory($baseDir, 0777, true);
+        }
+        
+        $filePath = $baseDir . '/' . date('Y-m-d') . '.log';
+    
+        return $filePath;
+    }
+
+    public function getMessage(Request $request, Response $response, $startTime = 0)
     {
         $files = (new Collection(iterator_to_array($request->files)))
             ->map([$this, 'flatFiles'])
             ->flatten();
-
+    
+        $responseTime = microtime(true) - $startTime;
+    
         return [
-            'method' => strtoupper($request->getMethod()),
-            'uri' => $request->getPathInfo(),
-            'body' => $request->except(config('http-logger.except')),
-            'headers' => $this->getSanitizer()->clean($request->headers->all(), config('http-logger.sanitize_headers')),
-            'files' => $files,
+            'timestamp' => gmdate('c'),
+            'request' => [
+                'method' => strtoupper($request->getMethod()),
+                'uri' => $request->getPathInfo(),
+                'full_url' => $request->fullUrl(),
+                'query_parameters' => $request->query(),
+                
+                'body' => $request->except(config('http-logger.except')),
+                'headers' => $this->getSanitizer()->clean(
+                    $request->headers->all(),
+                    config('http-logger.sanitize_headers')
+                ),
+                'files' => $files,
+            ],
+            'client' => [
+                'client_ip' => $request->ip(),
+                'user_agent' => $request->header('User-Agent'),
+                'authenticated_user' => optional($request->user())->only(['id', 'email']),
+            ],
+            'response' => [ 
+                'response_status' => $response->status(),
+                'response_headers' => $response->headers->all(),
+                'response_time' => $responseTime,
+            ],
+            'version' => [
+                'release_version' => config('http-logger.release_version'), // String
+                'commit_hash' => config('http-logger.commit_hash'),         // String
+                'commit_author' => config('http-logger.commit_author'),     // String
+                'commit_date' => config('http-logger.commit_date'),         // String
+                'author' => config('http-logger.author'), 
+            ]
         ];
-    }
-
-    protected function formatMessage(array $message)
-    {
-        $bodyAsJson = json_encode($message['body']);
-        $headersAsJson = json_encode($message['headers']);
-        $files = $message['files']->implode(',');
-
-        return "{$message['method']} {$message['uri']} - Body: {$bodyAsJson} - Headers: {$headersAsJson} - Files: " . $files;
     }
 
     public function flatFiles($file)
